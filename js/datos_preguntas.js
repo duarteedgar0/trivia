54ce7e24c1ef1fbeb77b10c22be525f91e448e11
edgar duarte <edgar.duarte@comunidad.ub.edu.ar>
2024-07-17 23:39:01 -0300

    feat: trivia final ub

---
+++ js/datos_preguntas.js
@@ -0,0 +1,80 @@
+/*
+Archivo: datos_preguntas.js
+Configuración de las preguntas del juego.
+*/
+
+let preguntas = [
+  {
+   texto_pregunta:'¿Cuál de estos elementos se suelen encontrar en la composición de la ALU?',
+    opciones:[
+      'Circuito Operacional y Registro de Estado',
+      'Registros de Entrada',
+      'Todos son correctos'
+    ],
+    imagen_src:'img/preguntas/alu.jpg',
+    respuesta_correcta: 'Todos son correctos'
+  },
+  {
+    texto_pregunta:'¿Qué partes componen la URL?',
+     opciones:[
+       'Protocolo y Nombre de dominio',
+       'Ruta y Parámetros',
+       'Todas son correctas'
+     ],
+     imagen_src:'img/preguntas/url-ub.jpg',
+     respuesta_correcta: 'Todas son correctas'
+   },
+   {
+    texto_pregunta:'¿Qué se utilizaba durante la primera generación de computadoras para el procesamiento?',
+     opciones:[
+       'Transistores',
+       'Tubos de vacío',
+       'Circuitos integrados'
+     ],
+     imagen_src:'img/preguntas/generacion.jpg',
+     respuesta_correcta: 'Tubos de vacío'
+   },
+   {
+    texto_pregunta:'¿Qué función cumple la memoria caché?',
+     opciones:[
+       'Mejorar el desempeño de la CPU y su velocidad',
+       'Funciona como motor grafico',
+       'Almacena el sistema operativo'
+     ],
+     imagen_src:'img/preguntas/cache.jpg',
+     respuesta_correcta: 'Mejorar el desempeño de la CPU y su velocidad'
+   },
+   {
+    texto_pregunta:'¿De qué se ocupan los controladores (o drivers) dentro de un sistema operativo?',
+     opciones:[
+       'Del arranque del sistema',
+       'Del control de los dispositivos de entrada y salida (E/S)',
+       'De la conexion entre los perifericos y el hardware.'
+     ],
+     imagen_src:'img/preguntas/controlador.jpg',
+     respuesta_correcta: 'De la conexion entre los perifericos y el hardware.'
+   },
+   {
+   texto_pregunta:'¿Cúal de las sifuientes corresponde a una conectividad de alcance local (casa, oficina, etc)? ',
+   opciones:[
+     'WLAN (Wireless Local Area Network)',
+     'LAN (Local Area Network)',
+     'PAN (Personal Area Network)'
+   ],
+   imagen_src:'img/preguntas/LAN.JPG',
+   respuesta_correcta: 'LAN (Local Area Network)'
+ },
+ {
+ texto_pregunta:'¿Cuál de las siguientes compañías fue pionera en la fabricación de mainframes?',
+ opciones:[
+   'IBM',
+   'Microsoft',
+   'Apple'
+ ],
+ imagen_src:'img/preguntas/trivia.png',
+ respuesta_correcta: 'IBM'
+},
+
+  
+];
+
